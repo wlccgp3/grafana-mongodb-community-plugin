be89c51b712e9d98c5149e9585d07e0a4612795b
miles <15070926843@163.com>
2024-05-16 19:06:54 +0800

    add grafana default func toObjectId

--- src/datasource.ts
+++ src/datasource.ts
@@ -4,7 +4,8 @@ import {
     DataQueryRequest, 
     DataQueryResponse, 
     MetricFindValue,
-    ScopedVars
+    ScopedVars,
+    TimeRange
 } from '@grafana/data';
 import {
     DataSourceWithBackend, 
@@ -12,27 +13,71 @@ import {
     frameToMetricFindValue
 } from '@grafana/runtime';
 import { MongoDBDataSourceOptions, MongoDBQuery, MongoDBQueryType, MongoDBVariableQuery } from './types';
+import { format } from 'date-fns'; // 使用 date-fns 进行日期格式化
 
 export class DataSource extends DataSourceWithBackend<MongoDBQuery, MongoDBDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MongoDBDataSourceOptions>) {
     super(instanceSettings);
   }
 
- applyTemplateVariables(query: MongoDBQuery, scopedVars: ScopedVars): Record<string, any> {
+  applyTemplateVariables(query: MongoDBQuery, scopedVars: ScopedVars): Record<string, any> {
     const templateSrv = getTemplateSrv();
-    return {
+    const processedQuery = {
       ...query,
       database: query.database ? templateSrv.replace(query.database, scopedVars) : '',
       collection: query.collection ? templateSrv.replace(query.collection, scopedVars) : '',
       aggregation: query.aggregation ? templateSrv.replace(query.aggregation, scopedVars, 'json') : ''
     };
+
+    return processedQuery;
   }
 
+  replaceCustomFunctions(aggregation: string, timeRange: TimeRange): string {
+    // 匹配所有自定义函数，包括可选的日期格式化字符串
+    const customFuncRegex = /\{\{\s*__(time_from|time_to):date(?::([^\s\}]+))?\s*\}\}/g;
+
+    return aggregation.replace(customFuncRegex, (match, p1, p2) => {
+      const timeValue = p1 === 'time_from' ? timeRange.from.toDate() : timeRange.to.toDate();
+
+      if (!p2 || p2 === 'iso') {
+        // 处理 `__time_from:date` 或 `__time_to:date` 或 `__time_from:date:iso` 的情况
+        return timeValue.toISOString();
+      }
+
+      switch (p2) {
+        case 'toObjectId':
+          return this.timeToObjectId(timeValue);
+        case 'seconds':
+          return Math.floor(timeValue.valueOf() / 1000).toString();
+        case 'milliseconds':
+          return timeValue.valueOf().toString();
+        default:
+          // 处理自定义日期格式化字符串
+          return format(timeValue, p2);
+      }
+    });
+  }
+
+  // Helper function to convert time to MongoDB ObjectId format
+  timeToObjectId(time: Date): string {
+    const timestamp = Math.floor(time.valueOf() / 1000).toString(16);
+    return timestamp + "0000000000000000";
+  }
 
   query(request: DataQueryRequest<MongoDBQuery>): Observable<DataQueryResponse> {
-      const templateSrv = getTemplateSrv();
-      templateSrv.updateTimeRange(request.range);
-      return super.query(request);
+    const timeRange = request.range;
+    const processedTargets = request.targets.map(target => {
+      const processedQuery = this.applyTemplateVariables(target, request.scopedVars);
+      processedQuery.aggregation = this.replaceCustomFunctions(processedQuery.aggregation, timeRange);
+      return processedQuery;
+    });
+
+    const modifiedRequest: DataQueryRequest<MongoDBQuery> = {
+      ...request,
+      targets: processedTargets as MongoDBQuery[],
+    };
+
+    return super.query(modifiedRequest);
   }
 
   async metricFindQuery(query: MongoDBVariableQuery, options?: any): Promise<MetricFindValue[]> {
@@ -51,13 +96,13 @@ export class DataSource extends DataSourceWithBackend<MongoDBQuery, MongoDBDataS
         autoTimeSort: false,
         schemaInference: false,
         schemaInferenceDepth: 0,
-    }
+    };
 
     let dataQuery = {
         ...options,
         targets: [target]
-    }
-    let dataQueryRequest = dataQuery as DataQueryRequest<MongoDBQuery>
+    };
+    let dataQueryRequest = dataQuery as DataQueryRequest<MongoDBQuery>;
 
     return lastValueFrom(
         this.query(dataQueryRequest)
